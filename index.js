4eff1a1c2d7125a14995a58d6b1fe169b7a6aef7
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-03-28 21:08:17 -0300

    Fix: resolve CORS policy

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ import postRoutes from "./routes/posts";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://user_admin:@kiraLeo2912@cluster1.qjai6.mongodb.net/memoriesDatabase?retryWrites=true&w=majority";
